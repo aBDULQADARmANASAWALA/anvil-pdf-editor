649b5844cb214d22588a4ec29c4951050f5446a1
aqmanasawala@gmail.com <aqmanasawala@gmail.com>
2024-08-23 13:25:34 +0000

    Created Picture1.png; Edited settings, Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import pymupdf
 
 
 class Form1(Form1Template):
@@ -8,3 +10,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    doc = pymupdf.open(file)
+    for pg_no in range(len(doc)):
+      doc[pg_no].insert_image(pymupdf.Rect(353, 87, 560, 106), filename = "_/theme/Picture1.png")
+    # doc.save("edited_file.pdf")
+
